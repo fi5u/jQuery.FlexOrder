258c764187de8c887c50f8a73cd5564e75e653d5
Tommy Fisher <tommybfisher@gmail.com>
2013-06-11 12:50:21 +0300

    Added targetContainer and targetPosition options, updated README and added LICENCE

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
-# jQuery.flexorder
-A jQuery plugin that simulates flex-order by taking a last child element and moving it to become the first child.
+# jquery.flexorder
+A jQuery plugin that simulates the order property from the [CSS Flexible box layout module](http://www.w3.org/TR/2012/CR-css3-flexbox-20120918/), by changing the flow order of an element on small viewports. In addition to its CSS equivilant, when flexed, jquery.flexorder can move the element into a different parent element than it originally had.
+
+*Be aware that, whereas the order CSS property only changes the visible flow, jquery.flexorder changes the DOM structure of the document; this can have accessibiliy implications.*
 
 ### Usage
 ```javascript
@@ -8,7 +10,9 @@ $('#selectorID').flexorder();
 `$('#selectorID')` is the element that should be flexed.
 
 ### Optional parameters
-* breakpoint (integer) - Point at which `$('#selectorID')` element moves back into the original flow.
+* breakpoint (integer) - Point at which `$('#selectorID')` element moves back into the original flow **default: 960**.
+* targetContainer (DOM element) - The element into which selector element should be moved to **default: parent of selector element**.
+* targetPosition (string: "start" / "end") - The point in the target container where the selector element should be placed **default: "start"**.
 
 ###License
-MIT [http://rem.mit-license.org](http://rem.mit-license.org)
+See LICENCE.md
--- jquery.flexorder.js
+++ jquery.flexorder.js
@@ -1,9 +1,18 @@
+/*
+ * jquery.flexorder.js 1.0
+ *
+ * Copyright 2013, Tommy Fisher http://www.tommyfisher.net
+ *
+ */
+
 (function($){
 
-    $.fn.flexorder = function( options ) {
 
+    $.fn.flexorder = function( options ) {
         var settings = {
-            breakpoint : 960
+            breakpoint : 960,
+            targetContainer : $(this).parent(),
+            targetPosition : "start"
         }
 
         if (options) {
@@ -11,15 +20,49 @@
         }
 
         return this.each(function(i, el) {
-            var winW = $(window).width();
-            var flexTarget = $(el).parent();
-            if( winW < settings.breakpoint && !$("body").hasClass("flexorder-flexed") ) {
-                $(el).prependTo(flexTarget);
-                $("body").addClass("flexorder-flexed");
-            } else if( winW >= settings.breakpoint && $("body").hasClass("flexorder-flexed") ) {
-                $(el).appendTo(flexTarget);
-                $("body").removeClass("flexorder-flexed");
+            /* remember selector's original order position */
+            var originalLocation = {};
+            var item = $(el).prev();
+            if( $(el).prev().length ) {
+                /* not originally first child */
+                originalLocation.prev = $(el).prev()[0];
+            } else {
+                /* originally a first child */
+                originalLocation.parent = $(el).parent()[0];
             }
+
+            initiateFlexorder = function() {
+                var winW = $(window).width();
+                var flexTarget = $(el).parent();
+                if( winW < settings.breakpoint && !$("body").hasClass("flexorder-flexed") ) {
+                    /* flex the order of the item */
+
+                    if( settings.targetPosition === "start" ) {
+                        $(el).prependTo(settings.targetContainer[0]);
+                    } else {
+                        $(el).appendTo(settings.targetContainer[0]);
+                    }
+                    $("body").addClass("flexorder-flexed");
+                } else if( winW >= settings.breakpoint && $("body").hasClass("flexorder-flexed") ) {
+                    /* return the flexed item back into the orignal flow */
+                    if( originalLocation.parent ) {
+                        /* element was a first child */
+                        $(originalLocation.parent).prepend(el);
+                    } else {
+                        /* element was not a first child */
+                        /* add a line break to preserve inline-block spacing */
+                        $(originalLocation.prev).after(el).after("\n");
+                    }
+                    $("body").removeClass("flexorder-flexed");
+                }
+            }
+
+            initiateFlexorder();
+
+            $(window).resize(function() {
+                initiateFlexorder();
+            });
         });
     }
+
 })( jQuery );
